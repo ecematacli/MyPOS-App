6a3d647a2dcfc2efb28beda88855d720cadb41af
Ecem Atacli <ecematacli@gmail.com>
2020-10-03 14:32:15 +0200

    stock-orders wip

--- src/pages/dashboardPage/hooks/useDashboardState.ts
+++ src/pages/dashboardPage/hooks/useDashboardState.ts
@@ -1,48 +1,48 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-import api from '../../../api';
+import api from '../../../api'
 import {
   InitialDates,
   Loading,
   TopSellingData,
   LastActivitiesData,
   RevenueData,
-  SaleStatsData
-} from '../types';
-import useAsyncError from '../../../common/hooks/useAsyncError';
+  SaleStatsData,
+} from '../types'
+import useAsyncError from '../../../common/hooks/useAsyncError'
 import {
   getInitialLastThirtyDays,
   getUnstatedDisplayOption,
   formatChartDate,
-  formatActivitiesData
-} from '../utils';
+  formatActivitiesData,
+} from '../utils'
 
 export default () => {
-  const { initialStart, initialEnd }: InitialDates = getInitialLastThirtyDays();
-  const throwError = useAsyncError();
+  const { initialStart, initialEnd }: InitialDates = getInitialLastThirtyDays()
+  const throwError = useAsyncError()
 
   const initialValues = {
     startDate: initialStart,
-    endDate: initialEnd
-  };
+    endDate: initialEnd,
+  }
 
-  const [loading, setLoading] = useState<Loading>({});
-  const [startDate, handleStartDateChange] = useState<Date | null>(null);
-  const [endDate, handleEndDateChange] = useState<Date | null>(null);
+  const [loading, setLoading] = useState<Loading>({})
+  const [startDate, handleStartDateChange] = useState<Date | null>(null)
+  const [endDate, handleEndDateChange] = useState<Date | null>(null)
   const [topSellingProducts, setTopSellingProducts] = useState<TopSellingData>({
     count: 0,
-    products: []
-  });
-  const [lastActivities, setLastActivities] = useState<LastActivitiesData>([]);
-  const [revenue, setRevenue] = useState<RevenueData>([]);
+    products: [],
+  })
+  const [lastActivities, setLastActivities] = useState<LastActivitiesData>([])
+  const [revenue, setRevenue] = useState<RevenueData>([])
   const [saleStats, setSaleStats] = useState<SaleStatsData>({
     webRevenue: 0,
     storeRevenue: 0,
     saleCount: 0,
-    soldProductCount: 0
-  });
+    soldProductCount: 0,
+  })
 
-  const [appliedFilters, setAppliedFilters] = useState(initialValues);
+  const [appliedFilters, setAppliedFilters] = useState(initialValues)
 
   // API Call helpers being used by all functions
   const getRequestParams = (
@@ -51,117 +51,98 @@ export default () => {
     endDate: Date,
     firstParam: boolean = false
   ) => {
-    const filters = { startDate, endDate };
+    const filters = { startDate, endDate }
 
     const params = Object.keys(filters)
       .reduce((arr, key) => {
         if (filters[key]) {
-          arr.push(`${key}=${filters[key].toISOString()}`);
+          arr.push(`${key}=${filters[key].toISOString()}`)
         }
-        return arr;
+        return arr
       }, [])
-      .join('&');
+      .join('&')
 
-    if (!params.length) return baseUrl;
+    if (!params.length) return baseUrl
 
     if (firstParam) {
-      return baseUrl + '?' + params;
+      return baseUrl + '?' + params
     } else {
-      return baseUrl + '&' + params;
+      return baseUrl + '&' + params
     }
-  };
+  }
 
-  const makeApiCall = async (
-    url: string,
-    name: string = '',
-    method: string = 'get'
-  ) => {
+  const makeApiCall = async (url: string, name: string = '', method: string = 'get') => {
     try {
-      setLoading(loading => ({ ...loading, [name]: true }));
-      const response = await api[method](url);
-      setLoading(loading => ({ ...loading, [name]: false }));
+      setLoading(loading => ({ ...loading, [name]: true }))
+      const response = await api[method](url)
+      setLoading(loading => ({ ...loading, [name]: false }))
 
-      return response.data;
+      return response.data
     } catch (e) {
-      setLoading(loading => ({ ...loading, [name]: false }));
-      throwError(new Error(e));
+      setLoading(loading => ({ ...loading, [name]: false }))
+      console.log(e)
     }
-  };
+  }
 
   // Dashboard Page API Caller functions
-  const fetchRevenueData = async (
-    displayOption: string,
-    start: Date,
-    end: Date
-  ) => {
-    let option: string;
+  const fetchRevenueData = async (displayOption: string, start: Date, end: Date) => {
+    let option: string
 
     if (!displayOption) {
-      option = getUnstatedDisplayOption(start, end);
+      option = getUnstatedDisplayOption(start, end)
     } else {
-      option = displayOption;
+      option = displayOption
     }
 
-    const url = getRequestParams(
-      `/stats/revenue-chart?option=${option}`,
-      start,
-      end
-    );
-    const data: RevenueData = await makeApiCall(url);
+    const url = getRequestParams(`/stats/revenue-chart?option=${option}`, start, end)
+    const data: RevenueData = await makeApiCall(url)
 
-    const formattedChartRevenue: RevenueData = formatChartDate(data, option);
+    const formattedChartRevenue: RevenueData = formatChartDate(data, option)
 
-    setRevenue(formattedChartRevenue);
-  };
+    setRevenue(formattedChartRevenue)
+  }
 
   const fetchSaleStats = async (start: Date, end: Date) => {
-    const url = getRequestParams(`/stats/sale-stats`, start, end, true);
+    const url = getRequestParams(`/stats/sale-stats`, start, end, true)
 
-    const data: SaleStatsData = await makeApiCall(url);
-    setSaleStats(data);
-  };
+    const data: SaleStatsData = await makeApiCall(url)
+    setSaleStats(data)
+  }
 
-  const fetchTopSellingProducts = async (
-    pageNumber: number = 1,
-    start: Date,
-    end: Date
-  ) => {
+  const fetchTopSellingProducts = async (pageNumber: number = 1, start: Date, end: Date) => {
     const url = getRequestParams(
       `/stats/top-selling-products?page=${pageNumber}&rowsPerPage=3`,
       start,
       end
-    );
+    )
 
-    const data: TopSellingData = await makeApiCall(url, 'topSellings');
+    const data: TopSellingData = await makeApiCall(url, 'topSellings')
 
-    setTopSellingProducts(data);
-  };
+    setTopSellingProducts(data)
+  }
 
   const fetchLastActivities = async () => {
-    const data: LastActivitiesData = await makeApiCall(
-      '/events/sales',
-      'activities'
-    );
-    const formattedActivities: LastActivitiesData = formatActivitiesData(data);
-    setLastActivities(formattedActivities);
-  };
+    const data: LastActivitiesData = await makeApiCall('/events/sales', 'activities')
+    const formattedActivities: LastActivitiesData = formatActivitiesData(data)
+    setLastActivities(formattedActivities)
+  }
 
   // Date Picker filter click event handlers
   const onDateSelection = () => {
-    setAppliedFilters({ startDate, endDate });
-    fetchRevenueData(null, startDate, endDate);
-    fetchSaleStats(startDate, endDate);
-    fetchTopSellingProducts(1, startDate, endDate);
-  };
+    setAppliedFilters({ startDate, endDate })
+    fetchRevenueData(null, startDate, endDate)
+    fetchSaleStats(startDate, endDate)
+    fetchTopSellingProducts(1, startDate, endDate)
+  }
 
   const onDateFilterClearing = () => {
-    handleStartDateChange(initialStart);
-    handleEndDateChange(initialEnd);
-    setAppliedFilters(initialValues);
-    fetchRevenueData('daily', initialStart, initialEnd);
-    fetchSaleStats(initialStart, initialEnd);
-    fetchTopSellingProducts(1, initialStart, initialEnd);
-  };
+    handleStartDateChange(initialStart)
+    handleEndDateChange(initialEnd)
+    setAppliedFilters(initialValues)
+    fetchRevenueData('daily', initialStart, initialEnd)
+    fetchSaleStats(initialStart, initialEnd)
+    fetchTopSellingProducts(1, initialStart, initialEnd)
+  }
 
   return {
     loading,
@@ -179,6 +160,6 @@ export default () => {
     fetchRevenueData,
     saleStats,
     fetchSaleStats,
-    revenue
-  };
-};
+    revenue,
+  }
+}
--- src/pages/dashboardPage/utils/index.ts
+++ src/pages/dashboardPage/utils/index.ts
@@ -1,79 +1,82 @@
-import differenceInDays from 'date-fns/differenceInDays';
-import sub from 'date-fns/sub';
+import differenceInDays from 'date-fns/differenceInDays'
+import sub from 'date-fns/sub'
 
-import { RevenueData, LastActivitiesData } from '../types';
-import { formatDate } from '../../../common/utils';
+import { RevenueData, LastActivitiesData } from '../types'
+import { formatDate } from '../../../common/utils'
 
 export const getUnstatedDisplayOption = (startDate: Date, endDate: Date) => {
-  const diff = differenceInDays(endDate, startDate);
+  const diff = differenceInDays(endDate, startDate)
 
   if (diff > 0 && diff <= 31) {
-    return 'daily';
+    return 'daily'
   }
 
   if (diff > 31 && diff <= 60) {
-    return 'weekly';
+    return 'weekly'
   }
 
   if (diff > 60 && diff <= 200) {
-    return 'monthly';
+    return 'monthly'
   }
 
-  return 'monthly';
-};
+  return 'monthly'
+}
 
 export const getDisabledOptions = (startDate: Date, endDate: Date) => {
   if (!startDate && endDate) {
-    return { monthly: true, weekly: false, daily: false };
+    return { monthly: true, weekly: false, daily: false }
   }
 
   if (startDate && !endDate) {
-    endDate = new Date();
+    endDate = new Date()
   }
 
-  const diff = differenceInDays(endDate, startDate);
+  const diff = differenceInDays(endDate, startDate)
 
   if (diff < 7) {
-    return { monthly: true, weekly: true, daily: false };
+    return { monthly: true, weekly: true, daily: false }
   }
 
   if (diff > 7 && diff <= 31) {
-    return { monthly: true, weekly: false, daily: false };
+    return { monthly: true, weekly: false, daily: false }
   }
   if (diff > 31 && diff <= 60) {
-    return { monthly: false, weekly: false, daily: false };
+    return { monthly: false, weekly: false, daily: false }
   }
 
   if (diff > 60 && diff <= 200) {
-    return { monthly: false, weekly: false, daily: true };
+    return { monthly: false, weekly: false, daily: true }
   }
 
   if (diff > 200) {
-    return { monthly: false, weekly: true, daily: true };
+    return { monthly: false, weekly: true, daily: true }
   }
-};
+}
 
 export const getInitialLastThirtyDays = () => {
-  const endDate = new Date();
-  const startDate = sub(endDate, { days: 30 });
+  const endDate = new Date()
+  const startDate = sub(endDate, { days: 30 })
 
-  return { initialStart: startDate, initialEnd: endDate };
-};
+  return { initialStart: startDate, initialEnd: endDate }
+}
 
 //Date formatter helpers
 export const formatActivitiesData = (lastActivities: LastActivitiesData) =>
-  lastActivities.map(action => ({
-    ...action,
-    created: formatDate(action.created, 'd MMMM y - p')
-  }));
+  lastActivities && lastActivities.length
+    ? lastActivities.map(action => ({
+        ...action,
+        created: formatDate(action.created, 'd MMMM y - p'),
+      }))
+    : []
 
 export const formatChartDate = (revenue: RevenueData, option: string) => {
   if (option === 'daily') {
-    return revenue.map(data => ({
-      ...data,
-      x: formatDate(data.x, 'd/M/y')
-    }));
+    return revenue
+    // return revenue.map(data => ({
+    //   ...data,
+    //   x: formatDate(data.x, 'd/M/y')
+    // }));
   } else {
-    return revenue;
+    return revenue
   }
-};
+}
